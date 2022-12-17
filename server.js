acc3628a66e3034b4aabba703327927198351c51
Ricky Aponte <Rickyapontephotography@gmail.com>
2022-12-16 22:58:37 -0500

    added some handlebars files

--- server.js
+++ server.js
@@ -33,5 +33,5 @@ app.set('view engine', 'handlebars');
 // app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log('Now listening'));
+    app.listen(PORT, () => console.log(`Now listening at http://localhost:${PORT} 🚀`));
 });
