150e190647536f88f9c048b06340579c148deac4
Ricky Aponte <Rickyapontephotography@gmail.com>
2022-12-19 13:05:45 -0500

    added the views pages as well as functionallity to the home routes

--- server.js
+++ server.js
@@ -30,8 +30,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-// app.use(routes);
+app.use(routes);
 
-sequelize.sync({ force: false }).then(() => {
+sequelize.sync({ force: false }).then(() => { 
     app.listen(PORT, () => console.log(`Now listening at http://localhost:${PORT} 🚀`));
 });
